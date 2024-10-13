fb6c6ddd3cd06657b18c0b5b5c3bf0bca79009c5
Javed Ali <Javedali90045@gmail.com>
2024-10-13 21:13:18 +0530

    I added color generating functionality

--- src/Components/ColorGenerator.jsx
+++ src/Components/ColorGenerator.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ColorGenerator.css'
 const ColorGenerator = () => {
+
+    const [typeOfColor, setTypeOfColor] = useState("RGB");
+    const [color, setColor] = useState("#000fff");
+
+    const randomColorUtility = (length) =>{
+        return Math.floor(Math.random()*length)
+    }
+    const handleCreateRandomColor = () =>{
+        const hex = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+        let hexColor = "#"
+        console.log(randomColorUtility(hex.length));
+        for(let i=0; i<6; i++)
+        {
+            hexColor+=hex[randomColorUtility(hex.length)];
+        }
+        setColor(hexColor)
+    }
+    const handleCreateRgbColor = () =>{
+
+        const r = randomColorUtility(255)
+        const g = randomColorUtility(255)
+        const b = randomColorUtility(255)
+        setColor(`rgb(${r},${g},${b})`)
+        
+    }
+
+    useEffect(()=>{
+        if(typeOfColor === "RGB")
+        {
+            handleCreateRgbColor()
+        }
+        else{
+            handleCreateRandomColor()
+        }
+    },[typeOfColor])
   return (
-    <div className='ColorGenrator'>
+    <div className='ColorGenrator' 
+        style={
+            {
+                background: color
+            }
+        }>
         <div className="btns">
-            <button>Create HEX Color</button>
-            <button>Create RGB Color</button>
-            <button>Generate Random Color</button>
+            <button onClick={()=>setTypeOfColor("HEX")}>Create HEX Color</button>
+            <button onClick={()=>setTypeOfColor("RGB")}>Create RGB Color</button>
+            <button onClick={typeOfColor === "HEX"? handleCreateRandomColor : handleCreateRgbColor}>Generate Random Color</button>
         </div>
         <div className="ColorTitleType">
-            <h1>HEX Color</h1>
+            <h1>{typeOfColor} Color</h1>
         </div>
         <div className="ColorCode">
-            <h2>#2261A9</h2>
+            <h2>{color}</h2>
         </div>
     </div>
   )
